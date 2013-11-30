066462bc4e078af157314e2b90f4db1551a6cf2d
Ryan Wilson <syndicated.life@gmail.com>
2013-11-29 21:45:53 -0800

    updated for new mahapps/expander control
    this was a new pull request to mahapps so the control may change; in
    which case I will update this repo as well

---
+++ FFXIVAPP.Plugin.Event/Converters/NameMultiValueConverter.cs
@@ -0,0 +1,24 @@
+﻿// FFXIVAPP.Plugin.Event
+// MultiValueConverter.cs
+// 
+// Copyright © 2013 ZAM Network LLC
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FFXIVAPP.Plugin.Event.Converters
+{
+    public class NameMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return String.Format("{0} ({1})", values[0], values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
